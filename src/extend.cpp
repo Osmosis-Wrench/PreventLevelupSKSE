32fa6a0fe2368cd15600ec8bd88f3149f73d4a7f
Rosso Porko <77623077+Osmosis-Wrench@users.noreply.github.com>
2022-07-11 02:33:34 +1000

    Revert "trying to get keyboard support to work"
    This reverts commit 0d635ba932ca907ecb4088664445c8d1e2e4a797.

--- src/extend.cpp
+++ src/extend.cpp
@@ -5,16 +5,16 @@ namespace extend
 	void StatsMenuEx::AcceptEx(RE::FxDelegateHandler::CallbackProcessor* a_processor)
 	{
 		originalAcceptFunction(this, a_processor); // like everything this is basically just stolen from Qudix. seriously one day I'll try and do something that he hasn't already done and i'll be so totall lost.
+		fxDelegate->callbacks.Remove("SetFadedIn");
+		a_processor->Process("SetFadedIn", [](const RE::FxDelegateArgs&) {
+			//nop
+		});
+
 		if (const auto input = RE::BSInputDeviceManager::GetSingleton()) {
-			if (!input->GetGamepad()->IsPressed(InputMap::GamepadMaskToKeycode(0x0100))) {
-				fxDelegate->callbacks.Remove("SetFadedIn");
-				a_processor->Process("SetFadedIn", [](const RE::FxDelegateArgs&) {
-					//nop
-				});
+			if (input->GetKeyboard()->IsPressed(42)) {
+				
 			}
 		}
-		//SKSE::InputMap
-		//!input->GetKeyboard()->IsPressed(42) || 
 	}
 
 	void StatsMenuEx::Install()
@@ -23,5 +23,31 @@ namespace extend
 		REL::Relocation<uintptr_t> vtbl(RE::VTABLE_StatsMenu[0]);
 		originalAcceptFunction = vtbl.write_vfunc(0x1, &AcceptEx);
 		logger::info("Installed!");
+	}
+
+	KeyInputEventHandler* KeyInputEventHandler::GetSingleton()
+	{
+		static KeyInputEventHandler singleton;
+		return std::addressof(singleton);
+	}
+
+	void KeyInputEventHandler::Register()
+	{
+		//static RE::BSWin32GamepadDevice test;
+		//static RE::BSInputDeviceManager inputManager;
+		//inputManager.GetSingleton();
+		if (const auto input = RE::BSInputDeviceManager::GetSingleton()) {
+			if (input->GetKeyboard()->IsPressed(26)) {
+				RE::DebugMessageBox("Test1");
+			}
+			if (input->GetKeyboard()->IsPressed(0x1A)) {
+				RE::DebugMessageBox("Test2");
+			}
+		}
+	}
+
+	RE::BSEventNotifyControl KeyInputEventHandler::ProcessEvent(RE::InputEvent* const* a_evn, RE::BSTEventSource<RE::InputEvent*>*)
+	{
+		return RE::BSEventNotifyControl::kContinue;
 	}
 }
